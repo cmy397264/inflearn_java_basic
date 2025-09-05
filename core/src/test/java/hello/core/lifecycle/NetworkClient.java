04356c102fdbac83789e48d227da98388ab77ae8
cmy397264 <cmy397264@naver.com>
2025-09-05 11:45:40 +0900

    인터페이스를 활용한 생명주기 콜백

---
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,42 @@
+package hello.core.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
+
+    private String url;
+
+    public NetworkClient(){
+        System.out.println("생성자 호출, url = " + url);
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void connect() {
+        System.out.println("connect : " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call : "  + url + "message = " + message);
+    }
+
+    public void disconnect() {
+        System.out.println("close: " + url);
+    }
+
+    //의존 관계 주입 후 바로 실행
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    // 종료 전 수행
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
+    }
+}
