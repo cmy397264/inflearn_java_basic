66808c4f3390f274f716cafef9d61514c2113bc1
cmy397264 <cmy397264@naver.com>
2025-09-05 17:18:51 +0900

    proxyMode를 활용한 코드 축소

--- core/src/main/java/hello/core/web/LogDemoController.java
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -3,7 +3,6 @@ package hello.core.web;
 import hello.core.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,7 +16,7 @@ public class LogDemoController {
     // 이때 MyLogger의 생명주기는 Request이기 때문에
     // Request 범위 안에서 생존하는데 실행 초기 Request는 요청되지 않았기 때문에 오류가 발생함.
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
+    private final MyLogger myLogger;
 
     //화면없이 글자로 응답을 반환 => ResponseBody
     @RequestMapping("log-demo")
@@ -25,7 +24,6 @@ public class LogDemoController {
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURI();
 
-        MyLogger myLogger = myLoggerObjectProvider.getObject();
         myLogger.setRequestURL(requestURL);
         myLogger.log("controller test");
 
